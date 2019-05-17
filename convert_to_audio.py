7cc1a5140e321f7b4d9fad42816b36230d898015
Yu Hao <yuhao89@live.cn>
2019-05-17 09:22:58 +0800

    add command line parameters

--- convert_to_audio.py
+++ convert_to_audio.py
@@ -1,15 +1,36 @@
-import moviepy.editor as mp
+#!/usr/bin/env python3
+import argparse
 from pathlib import Path
 
-source_dir = Path("C:/Users/yuhao/Desktop/Early Middle Ages/")
-target_dir = Path("C:/Users/yuhao/Desktop/Early Middle Ages/converted_audio")
+import moviepy.editor as mp
 
-video_files = source_dir.glob("*.mp4")
+__author__ = 'yuhao'
 
-audio_files = target_dir.glob("*.mp3")
-names = [a.stem for a in audio_files]
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
-for file in video_files:
-    if file.stem not in names:
-        clip = mp.VideoFileClip(str(file))
-        clip.audio.write_audiofile(str(target_dir/ (file.stem + ".mp3")))
+
+def convert(source_dir, target_dir):
+    # print(source_dir)
+    # print(target_dir)
+
+    source_dir = Path(source_dir)
+    target_dir = Path(target_dir)
+
+    video_files = source_dir.glob("*.mp4")
+
+    audio_files = target_dir.glob("*.mp3")
+    names = [a.stem for a in audio_files]
+
+    for file in video_files:
+        if file.stem not in names:
+            clip = mp.VideoFileClip(str(file))
+            clip.audio.write_audiofile(str(target_dir/ (file.stem + ".mp3")))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--source_dir', required=True)
+    parser.add_argument('-d', '--target_dir', required=True)
+    args = parser.parse_args()
+
+    convert(args.source_dir, args.target_dir)
